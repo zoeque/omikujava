98c07ea9f090d99f1d2a3d212aed0a3f9521742b
zoeque <121230485+zoeque@users.noreply.github.com>
2024-01-12 19:44:42 +0900

    update the front end

--- front/omikujava/src/App.tsx
+++ front/omikujava/src/App.tsx
@@ -1,26 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import RequestButton from './RequestButton'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(null);
+
+  const handleResult = (res) => {
+    setResult(res);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>今年の運勢は</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {/* ここでRequestButtonのGETに対し得られたresultを表示する */}
+        <RequestButton onResult={handleResult} />
+        {result && <div>{result}</div>}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
