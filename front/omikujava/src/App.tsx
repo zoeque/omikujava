68dd8a406fdcd2c437eca6d08686d4c8d987d590
zoeque <121230485+zoeque@users.noreply.github.com>
2024-01-19 12:33:06 +0900

    modified the bug that disappered the all text

--- front/omikujava/src/App.tsx
+++ front/omikujava/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [result, setResult] = useState(null);
 
   const handleResult = (res) => {
-    setResult(res);
+    setResult(res.result);
   }
 
   return (
@@ -14,14 +14,11 @@ function App() {
       <h1>今年の運勢は</h1>
       <div className="card">
         {/* ここでRequestButtonのGETに対し得られたresultを表示する */}
+        {result && <div><h1>{result}</h1></div>}
         <RequestButton onResult={handleResult} />
-        {result && <div>{result}</div>}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Created by <a href="https://github.com/zoeque">zoeque</a>
       </p>
     </>
   )
